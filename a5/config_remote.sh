934509c0d6874e89907b56e420d29eeafb39dd40
Dat Nguyen <lonelyicevalley@icloud.com>
2019-03-23 09:52:11 +0700

    a5 push to github best model each epoch

--- a5/config_remote.sh
+++ a5/config_remote.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 pip install numpy --upgrade
 pip install -q torch==1.0.0 torchvision
+sudo apt-get install git-lfs
+git lfs install
 git clone https://github.com/nguyenvdat/CS224.git
+git clone https://github.com/nguyenvdat/model.git
+cd model
+git checkout CS224
+cd ../
+cp model/CS224/a5/model.bin CS224/a5
+cp model/CS224/a5/model.bin.optim CS224/a5
 cd CS224
 git checkout a5
 cd a5
